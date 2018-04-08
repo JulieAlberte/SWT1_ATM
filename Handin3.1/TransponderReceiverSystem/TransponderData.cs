938a4a25e0798ddeb63f3c455d5033abeeace246
Susanne Olsen <susanne_olsen@live.dk>
2018-04-08 12:09:59 +0200

    Something

--- Handin3.1/TransponderReceiverSystem/TransponderData.cs
+++ Handin3.1/TransponderReceiverSystem/TransponderData.cs
@@ -20,17 +20,17 @@ namespace TransponderReceiverSystem
             _y_coordinate = y_coordinate;
             _altitude = altitude;
             _timestamp = timestamp;
-            //Print();
+            Print();
         }
 
-        //public void Print()
-        //{
-        //    Console.WriteLine(_tag);
-        //    Console.WriteLine(_x_coordinate);
-        //    Console.WriteLine(_y_coordinate);
-        //    Console.WriteLine(_altitude);
-        //    Console.WriteLine(_timestamp);
-        //}
+        public void Print()
+        {
+            Console.WriteLine(_tag);
+            Console.WriteLine(_x_coordinate);
+            Console.WriteLine(_y_coordinate);
+            Console.WriteLine(_altitude);
+            Console.WriteLine(_timestamp);
+        }
 
     }
 }
