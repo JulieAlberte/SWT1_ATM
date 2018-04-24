543eeef9a1113e44ac96d5e2013b9e8c6d4f6f21
Emma Rolsted <emma-k-r@hotmail.com>
2018-04-24 09:17:45 +0200

    Seperation...

---
+++ ATM/ATMClasses/SeperationEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMClasses
+{
+    class SeperationEvent
+    {
+        public string TAG1 { get; set; }
+        public string TAG2 { get; set; }
+        public DateTime TimeOfEvent { get; set; }
+
+    }
+}
---
+++ ATM/ATMClasses/TrackSeperation.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ATMClasses.Data;
+using ATMClasses.Interfaces;
+
+namespace ATMClasses
+{
+    class TrackSeperation
+    {
+        private readonly SeperationEvent _seperationEvent;
+        private readonly ISeperationEventController _seperationEventController;
+        private readonly List<SeperationEvent> _seperationEventsList;
+
+        public TrackSeperation(SeperationEvent seperationEvent, ISeperationEventController seperationEventController)
+        {
+            _seperationEvent = seperationEvent;
+            _seperationEventController = seperationEventController;
+            _seperationEventsList = new List<SeperationEvent>();
+        }
+
+        void checkForSeperation(List<TrackData> trackDatalList)
+        {
+            for (var i = 0; i < trackDatalList.Count; i++)
+            {
+                for (int j = 0; j < trackDatalList.Count; j++)
+                {
+                    // If there is more then 2 trackobjects in list and the tracks tags not are the same
+                    if (trackDatalList.Count >= 2 && trackDatalList[i].Tag != trackDatalList[j].Tag)
+                    {
+                        //Coordinates to the to tracks we are investegating 
+                        double x1 = trackDatalList[i].X;
+                        double x2 = trackDatalList[j].X;
+                        double y1 = trackDatalList[i].Y;
+                        double y2 = trackDatalList[j].Y;
+
+                        //Horizontal seperation less than 5000 meters
+                        //Formula: distance = sqrt((x1-x2)^2+(y1-y2)^2)
+                        var horizontalDistance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+                        //vertical seperation less than 300 meters
+                        var verticalDistance = Math.Abs(trackDatalList[i].Altitude - trackDatalList[j].Altitude);
+
+                        if (horizontalDistance < 5000 && verticalDistance < 300)
+                        {
+                            var timeOfEvent = trackDatalList[i].Timestamp > trackDatalList[j].Timestamp
+                                ? trackDatalList[i].Timestamp
+                                : trackDatalList[j].Timestamp;
+
+                            _seperationEvent.TAG1 = trackDatalList[i].Tag;
+                            _seperationEvent.TAG2 = trackDatalList[i].Tag;
+                            _seperationEvent.TimeOfEvent = timeOfEvent;
+
+                            _seperationEventsList.Add(_seperationEvent);
+                            _seperationEventController.CoflictDetected(_seperationEventsList);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
